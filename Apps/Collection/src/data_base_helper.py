bcd72849648c1636075ff81346d5b62bd553a193
maxeffortcode <bultman82@gmail.com>
2022-04-03 15:17:05 -0500

    updated the data_base_helper file

--- Apps/Collection/src/data_base_helper.py
+++ Apps/Collection/src/data_base_helper.py
@@ -2,13 +2,16 @@
 #todo make funvtions that allow for easy transfer of csv's to psql table
 #works just need to install 'pip install psycopg2-binary'
 #python3.10 -m pip install --upgrade setuptools
+#python3.10 -m Apps.Collection.src.data_base_helper
+#sudo -u postgres -i
+#CREATE USER max WITH PASSWORD 'password';
+
 import psycopg2
 
-
-
-conn = psycopg2.connect("dbname=central_db user=postgres password=postgres")
+conn = psycopg2.connect(database="dummy", user = "max", password = "password", host = "127.0.0.1", port = "5432")
 
 print(conn)
+
 def csv_to_file(filename, path):
     pass
 
