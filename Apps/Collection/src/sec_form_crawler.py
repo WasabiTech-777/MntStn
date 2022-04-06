e85d8e66e565cad80637e82e872586b198d7b2f4
rubickz <57612098+rubickz@users.noreply.github.com>
2022-04-05 23:44:23 -0500

    10k filing processing feature
    * 10k CSV generated for one company, test processing others
    
    * debugging
    
    * Testing 10k
    
    * processed 10ks properly, just need to fix reindexing of header issue
    
    * processed 10ks properly, just need to fix reindexing of header issue
    
    * 10k filings processed
    
    * fix merge conflict

--- Apps/Collection/src/sec_form_crawler.py
+++ Apps/Collection/src/sec_form_crawler.py
@@ -20,19 +20,26 @@ with open(edgarIndexFilePath) as file:
     for line in itertools.islice(file, 11, None):
         splitLineCompanyInfo = line.strip().split("|")
 
-        if(splitLineCompanyInfo[2] == "13F-HR"):
+        companyName = splitLineCompanyInfo[1].strip()
+        companyName = companyName.replace(' ', '_')
+        companyFiling = splitLineCompanyInfo[2]
+
+        if(companyFiling == "13F-HR"):
+            companyInfoTuple = (companyName, companyFiling, qtr, yr) 
             fileCounter13fhr += 1
-            logger.info(f"Processing 13F-HR for : {splitLineCompanyInfo[1]}\n")
+            logger.info(f"Processing 13F-HR for : {companyName}\n")
             filingFile = sec_Api.get13FHRFilingForCompanyApi(splitLineCompanyInfo)
             time.sleep(1/10)
             helper().process_13f_hr(filingFile)
 
-        elif(splitLineCompanyInfo[2] == "10-K"):
+        elif(companyFiling == "10-K"):
+            companyInfoTuple = (companyName, companyFiling, qtr, yr)
             fileCounter10k += 1
-            logger.info(f"Processing 10-K for : {splitLineCompanyInfo[1]}\n")
+            logger.info(f"Processing 10-K for : {companyName}\n")
             filingFile = sec_Api.get10kFilingForCompanyApi(splitLineCompanyInfo)
             time.sleep(1/10)
-            helper.process_10k(filingFile, sec_Api)
+        
+            helper.process_10k(filingFile, sec_Api, companyInfoTuple)
 
 logger.info("Processed " + str(fileCounter13fhr) + " 13F-HR files in master file.")
 logger.info("Processed " + str(fileCounter10k) + " 10k files in master file.")
