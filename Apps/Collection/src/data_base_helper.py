ec8050d7e7eaadb3e30f6ca37d5d2261b7353895
maxeffortcode <bultman82@gmail.com>
2022-04-08 19:07:08 -0500

    cleaned and updated the sql cvs parser

--- Apps/Collection/src/data_base_helper.py
+++ Apps/Collection/src/data_base_helper.py
@@ -11,14 +11,6 @@
 import psycopg2
 
 
-conn = psycopg2.connect(database="dummy", user = "max", password = "password", host = "127.0.0.1", port = "5432")
-
-print(conn)
-
-
-conn.autocommit = True
-cursor = conn.cursor()
-
 
 class sql_parser:
     def __init__(self, nameOfIssuer, cusip, value, shares, sshPrnamtType,
@@ -36,37 +28,52 @@ class sql_parser:
         self.soleVotingAuthority = soleVotingAuthority
         self.sharedVotingAuthority = sharedVotingAuthority
         self.noneVotingAuthority = noneVotingAuthority
-    
+
+        
+    def connect_to_database(self):
+        conn = psycopg2.connect(database="dummy", user = "max", password = "password", host = "127.0.0.1", port = "5432")
+        print(conn)
+        conn.autocommit = True
+        cursor = conn.cursor()
+        return conn
+
+        
     def csv_to_psql_company_table_generator(self):
+        conn = self.connect_to_database()
+        cursor = conn.cursor()
+
         sql_command = f"CREATE TABLE {self.nameOfIssuer}(company_name varchar(63),\
             cusip int, value int, shares int, sshPrnamtType varchar(30), putCall varchar(16) ,\
             investmentDiscretion varchar(12), otherManager varchar(99), soleVotingAuthority int,\
             sharedVotingAuthority int, noneVotingAuthority int);"
+        
+        try:
+            cursor.execute(sql_command)
+        
+        except:
+            print("failed")
+            
+        finally:
+            print(f"Table : {self.nameOfIssuer} created succesfully.")
     
+        conn.commit()
+        conn.close()
+        
+    
+    def store_csv_to_table(self, file_location):
+        conn = self.connect_to_database()
+        cursor = conn.cursor()
+        f = open(file_location, 'r')
+        cursor.copy_from(f, f'{self.nameOfIssuer}', sep=',')
+        cursor.execute(f"select * from {self.nameOfIssuer};")
+        cursor.fetchall()
+        conn.commit()
+        conn.close()
+        
 
-
-
-#ARGENX SE,04016X101,9766,27889,SH,,DFND,1,2,3,4,27889,0,0
-sql = '''CREATE TABLE TEMPTEST(company_name varchar(63),\
-    filling_date varchar(30),\
-    num1 int, num2 int, cmp_ticker varchar(30), varnum varchar(30) ,\
-    cmp_ticker2 varchar(30), num11 int, num12 int, num13 int, num14 int,\
-    num15 int, num16 int, num17 int);'''
-  
-
-
-#cursor.execute(sql)
-f = open(r'Apps/Collection/src/resources/13F-HR-parsed-data.csv', 'r')
-cursor.copy_from(f, 'temptest', sep=',')
-cursor.execute("select * from temptest;")
-cursor.fetchall()
 
   
-  
-conn.commit()
-conn.close()
-
-
+print("afasdfasd")
 
 
 def csv_to_psql_data_table(csv_file, data_base="dummy", 
