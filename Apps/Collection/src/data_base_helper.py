e91660567d08769ddceccb48b8d377ed2ef1f0fa
maxeffortcode <bultman82@gmail.com>
2022-04-11 18:20:42 -0500

    small update to success message

--- Apps/Collection/src/data_base_helper.py
+++ Apps/Collection/src/data_base_helper.py
@@ -26,11 +26,7 @@ def csv_to_sql(conn, csv_file_path):
     
     cursor = conn.cursor()
     directory = os.path.abspath(csv_file_path)
-
     table_name = re.sub("[^a-zA-Z]+", "", directory.split('/')[-6])
-    print(f"table name : {table_name} \n")
-    #grabs the last folder in the csv file path
-    #sql_command = f'CREATE TABLE {os.path.abspath(csv_file_path)}('
     sql_command = f'CREATE TABLE {table_name}('
     
     with open(csv_file_path) as file:
@@ -39,7 +35,6 @@ def csv_to_sql(conn, csv_file_path):
             headers = line
             print(f"headers: {headers}\n\n")
             break
-
 
     for column in headers:
         #varchar does not pad and is the most versitile. could be changed in future to save space
@@ -51,7 +46,7 @@ def csv_to_sql(conn, csv_file_path):
     try:
         print(f"attempting to run: {sql_command} \n")
         cursor.execute(sql_command)
-        print(f"Table : {os.path.basename(csv_file_path)} created succesfully.\n")
+        print(f"Table : {table_name} created succesfully.\n")
         
     except:
         print("failed")
@@ -63,4 +58,4 @@ def csv_to_sql(conn, csv_file_path):
 
 if __name__ == "__main__":
     conn = connect_to_database()
-    csv_to_sql(conn, '/home/max/MntStn/Apps/Collection/src/resources/companies/ACME,_LLC/filings/13f-hr-filing/2022/1/13f-hr-data.csv')
+    csv_to_sql(conn, '/home/max/MntStn/Apps/Collection/src/resources/companies/Yakira_Capital_Management,_Inc./filings/13f-hr-filing/2022/1/13f-hr-data.csv')
